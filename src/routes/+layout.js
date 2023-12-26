ce972667eb4364583b74046bd754fbae1c7f8d74
Viliina Pajuniemi <vpajuniemi@gmail.com>
2023-12-26 15:57:01 +0200

    Jatkoa: joo siis ilman suurempia selityksiä, mun git töksähti ja pahasti... ei suostunut vanhaa repoo enää päivittään enkä ymmärrä miksi enkä halunnu tuhlata viimeisii päivii että etsin syyn joten tässä uusi repo (kolmannen kerran)

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,5 +1,6 @@
 export async function load({fetch}){
     //console.log("+layout.js")
+    
     // haetaan molemmat APIt
     const coursesAPI = await fetch("https://luentomuistiinpano-api.netlify.app/.netlify/functions/courses");
     const notesAPI = await fetch("https://luentomuistiinpano-api.netlify.app/.netlify/functions/notes");
