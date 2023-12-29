817cd8dd4f59e3e26dcdd7adde1472ef0704f466
Viliina Pajuniemi <vpajuniemi@gmail.com>
2023-12-29 18:31:09 +0200

    Delete - noten poisto alkuun, en ole varma ehdinkö tekemään mutta ainakin ajatustasolla että mitä voisi olla?

--- src/routes/Note.svelte
+++ src/routes/Note.svelte
@@ -6,9 +6,13 @@
     export let timestamp;
      
     const {id: courseID, name: courseName} = course
+
+    function deleteNote(){
+        console.log("Poista note: ", {id})
+    }
 </script>
     
-<div>
+<div class="note">
     <p>Course name: {courseName} // ID: {courseID}</p>
     <p>{timestamp}</p>
     {#if text !== undefined}
@@ -16,7 +20,7 @@
     {:else}
         <p>Ei muistiinpanoja!</p>  
     {/if}
-        
+ <button on:click={deleteNote}>Poista note!</button>
 </div>
     
 <style>
@@ -26,5 +30,20 @@
         background-color: rgb(168, 166, 145);
         border-radius: 10px;
         padding: 10px;
+    }
+    .note:hover{
+        background-color: red;
+    }
+    .note:hover button{
+        display: block;
+    }
+    button{
+        margin-top: 1rem;
+      padding: 5px 10px;
+      cursor: pointer;
+      background-color: #fff;
+      border: 1px solid #000;
+      border-radius: 5px;
+      display: none;
     }
 </style>
